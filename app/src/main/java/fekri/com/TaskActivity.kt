eb12198d08aa58ef3546e6376cd795302ffffadb
root <fekri.mohammadreza86@gmail.com>
2022-12-29 14:30:10 +0330

    added activities and icons

---
+++ app/src/main/java/fekri/com/TaskActivity.kt
@@ -0,0 +1,15 @@
+package fekri.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fekri.com.databinding.ActivityTaskBinding
+
+class TaskActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTaskBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
